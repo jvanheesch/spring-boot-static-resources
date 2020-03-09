b806749e238fc6a761e20704674d5e19b0b0c99b
jvanheesch <joris.vanheesch@ae.be>
2020-03-09 18:01:48 +0100

    k

--- src/main/java/com/github/jvanheesch/ForwardController.java
+++ src/main/java/com/github/jvanheesch/ForwardController.java
@@ -2,7 +2,6 @@ package com.github.jvanheesch;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ForwardController {
