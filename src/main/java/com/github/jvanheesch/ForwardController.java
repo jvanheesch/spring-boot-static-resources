4c714f9cb352175dbf5eecb6026ca150db8b14d6
jvanheesch <joris.vanheesch@ae.be>
2020-03-10 19:33:45 +0100

    Reroute assets example.

--- src/main/java/com/github/jvanheesch/ForwardController.java
+++ src/main/java/com/github/jvanheesch/ForwardController.java
@@ -3,11 +3,19 @@ package com.github.jvanheesch;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class ForwardController {
     @GetMapping("/range/**")
     public String forwardRange() {
         return "forward:/my-angular-app/dist/index.html";
+    }
+
+    @GetMapping("/range/assets/**")
+    public String forwardAssets(HttpServletRequest request) {
+        String assetsPath = request.getServletPath().replaceFirst("/range", "");
+        return "forward:/my-angular-app/dist" + assetsPath;
     }
 
     @GetMapping("/imon")
